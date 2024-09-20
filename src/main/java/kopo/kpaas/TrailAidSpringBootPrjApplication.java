280408d1ed01f1685d72f0832f62ab98f8a60fbc
tyson0922 <taeseung92l@gmail.com>
2024-09-20 20:31:10 +0900

    Initial commit

---
+++ src/main/java/kopo/kpaas/TrailAidSpringBootPrjApplication.java
@@ -0,0 +1,13 @@
+package kopo.kpaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailAidSpringBootPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrailAidSpringBootPrjApplication.class, args);
+    }
+
+}
